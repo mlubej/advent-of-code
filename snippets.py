a3abd00909ffc86304c4dd69466611f073c9b44c
Matic Lubej <matic.lubej@sinergise.com>
2022-12-16 17:45:55 +0100

    add snippets

---
+++ snippets.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+# https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm#Algorithm
+def find_optimal_path_djikstra(array, fill_value=99999):
+    current = (0, 0)
+    end = tuple(np.array(array.shape) - [1, 1])
+    cost_from_zero = np.full_like(array, fill_value=fill_value, dtype=int)
+    cost_from_zero[current] = 0
+    unvisited = {(x, y) for x in range(array.shape[1]) for y in range(array.shape[0])}
+
+    while True:
+        neighbors = set((current[0] + x, current[1] + y) for x, y in [[-1, 0], [1, 0], [0, -1], [0, 1]])
+        neighbors &= unvisited
+
+        ccost = cost_from_zero[current]
+        for nb in neighbors:
+            if ccost + array[nb] < cost_from_zero[nb]:
+                cost_from_zero[nb] = ccost + array[nb]
+
+        unvisited -= {current}
+        init_cands = {node for node in unvisited if cost_from_zero[node] == fill_value}
+        if current == end or len(unvisited) == len(init_cands):
+            break
+
+        current = sorted(unvisited - init_cands, key=lambda x: cost_from_zero[x])[0]
+
+    return cost_from_zero[end]
