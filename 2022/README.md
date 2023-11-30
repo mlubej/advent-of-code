1716d7a10f3d648137b7e0088c80f3a420a948b8
Matic Lubej <matic.lubej@sinergise.com>
2023-11-30 22:27:55 +0100

    shuffle tasks for 2021 and 2022

---
+++ 2022/README.md
@@ -0,0 +1,49 @@
+# Advent of Code 2022
+
+This is my repo for collecting tasks for AoC.
+
+I added a script that creates a directory for the task you're working on and loads a notebook from a template. The notebook already contains the basic structure and uses [aoc-data](https://github.com/wimglenn/advent-of-code-data) for importing the input data (check the link for instructions.)
+
+The scripts assumes you have Visual Studio Code installed and the `code` CLI binding available.
+
+### Requirements
+
+First install all the reqirements from the `requirements.txt` file with
+
+```bash
+pip install -r requirements.txt
+```
+
+### Usage
+
+Running the script without arguments prints out help:
+
+```
+Usage:  [OPTIONS]
+
+Options:
+  --year INTEGER  Year of Advend of Code event.
+  --day INTEGER   Day of Advent of Code event.
+  --help          Show this message and exit.
+```
+
+To start working on a task, simply run the following:
+
+```bash
+python aoc-setup.py --year 2021 --day 20
+
+# you can even create an alias in your ~/.zshrc or ~/.bashrc
+# alias aoc='python ~/path/to/repo/aoc-setup.py'
+# and then do just
+# aoc --year 2021 --day 20
+```
+
+This will create the following directory and files in the github repo and open VSCode in the appropriate location
+
+```
+.
+└── advent-of-code
+    └── 2021
+        └── day-20
+            └── task.ipynb
+```
---
+++ 2022/snippets.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+
+# https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm#Algorithm
+def find_optimal_path_djikstra(array, fill_value=99999):
+    current = (0, 0)
+    end = tuple(np.array(array.shape) - [1, 1])
+    cost_from_zero = np.full_like(array, fill_value=fill_value, dtype=int)
+    cost_from_zero[current] = 0
+    unvisited = {(x, y) for x in range(array.shape[1]) for y in range(array.shape[0])}
+
+    while True:
+        neighbors = set((current[0] + x, current[1] + y) for x, y in [[-1, 0], [1, 0], [0, -1], [0, 1]])
+        neighbors &= unvisited
+
+        ccost = cost_from_zero[current]
+        for nb in neighbors:
+            if ccost + array[nb] < cost_from_zero[nb]:
+                cost_from_zero[nb] = ccost + array[nb]
+
+        unvisited -= {current}
+        init_cands = {node for node in unvisited if cost_from_zero[node] == fill_value}
+        if current == end or len(unvisited) == len(init_cands):
+            break
+
+        current = sorted(unvisited - init_cands, key=lambda x: cost_from_zero[x])[0]
+
+    return cost_from_zero[end]
+
+
+def unify_ranges(ranges):
+    ranges = sorted(map(list, ranges))
+
+    new_ranges = [ranges[0]]
+    for cs, ce in ranges[1:]:
+        ps, pe = new_ranges[-1]
+        if cs <= pe:
+            new_ranges[-1][1] = max(ce, pe)
+        else:
+            new_ranges.append([cs, ce])
+
+    return new_ranges
+
+
+# https://www.codespeedy.com/inter-convert-decimal-and-any-base-using-python/
+def dec2base(num,base):  #Maximum base - 36
+    base_num = ""
+    while num>0:
+        dig = int(num%base)
+        if dig<10:
+            base_num += str(dig)
+        else:
+            base_num += chr(ord('A')+dig-10)  #Using uppercase letters
+        num //= base
+    base_num = base_num[::-1]  #To reverse the string
+    return base_num
