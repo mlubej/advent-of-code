b6a087dc9d0cb44df02e99a25421477183a09f43
Matic Lubej <matic.lubej@sinergise.com>
2023-12-02 18:50:55 +0100

    add solution for day 2

---
+++ 2023/days/day-02/task.pl
@@ -0,0 +1,44 @@
+use feature 'say';
+use String::Util qw(trim);
+use Time::HiRes  qw( time );
+
+use List::Util 'sum', 'all', 'max';
+
+my $begin_time = time();
+open( my $file, "<", $ARGV[0] ) or die "Can't open input file";
+
+# configuration
+my ( $redmax, $greenmax, $bluemax ) = ( 12, 13, 14 );
+
+my @valid_ids = ();
+my @powers    = ();
+while ( my $line = <$file> ) {
+    my $line = trim $line;
+
+    # get id
+    my ( $id, $sets ) = split( ':', $line );
+    my $id     = ( $id =~ /(\d+)/ )[0];
+    my @reds   = $sets =~ /(\d+) red/g;
+    my @greens = $sets =~ /(\d+) green/g;
+    my @blues  = $sets =~ /(\d+) blue/g;
+
+    # part 1
+    if ( ( all { $_ <= $redmax } @reds ) && ( all { $_ <= $greenmax } @greens ) && ( all { $_ <= $bluemax } @blues ) ) {
+        push @valid_ids, $id;
+    }
+
+    # part 2
+    push @powers, max(@reds) * max(@greens) * max(@blues);
+
+}
+
+close $file;
+
+# Part 1
+say sum @valid_ids;
+
+# Part 2
+say sum @powers;
+
+my $end_time = time();
+printf( "Elapsed time %.8fs\n", $end_time - $begin_time );
